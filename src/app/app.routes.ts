c36b7bba64acd95c2d340897fbfb927c7006a082
LZYLRRY <162039530+LZYLRRY@users.noreply.github.com>
2024-04-24 21:31:52 +0200

    first angular project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { VerkiezingenComponent } from './verkiezingen/verkiezingen.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        component: HomeComponent,
+        path: ''
+    },
+    {
+        component: AboutComponent,
+        path: 'about'
+    },
+    {
+        component: VerkiezingenComponent,
+        path: 'verkiezingen'
+    },
+];
